c2beb092563ae961b7ee0a6151ba4d409eb6e053
dci-student <simaafshar1@gmail.com>
2023-06-06 14:30:10 +0200

    finished

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import passport from "passport";
+import  initialize  from "./routes/passport-config.js";
+import userRoutes from './routes/userRoutes.js'
+
+
+dotenv.config();
+initialize(passport);
+const PORT = 4789
+const app = express();
+
+app.use(express.json());
+// is configuring the Express middleware to handle URL-encoded form data.
+// in version 4.16.0 and then it is not necessary because by default it is false
+app.use(express.urlencoded({ extended: false }));
+
+
+// data base 
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log("we are connected to the database.");
+  })
+  .catch((error) => {
+    console.log("an error occurred while connecting ot the db", error);
+  });
+
+// routes
+app.use('/', userRoutes)
+
+app.listen(PORT, () => console.log(`The webserver is running on port ${PORT}`));
